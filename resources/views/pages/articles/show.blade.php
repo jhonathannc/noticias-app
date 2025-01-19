b462a59140d59a5d456f69d0d01ba77247ed7c1d
jhonathannc <jhonathannc@gmail.com>
2025-01-18 22:36:07 -0300

    feat: showning category name at article view

--- resources/views/pages/articles/show.blade.php
+++ resources/views/pages/articles/show.blade.php
@@ -4,6 +4,10 @@
     <h1 class="font-bold text-xl">{{ $article->title }}</h1>
 
     <span>{{ $article->content }}</span>
+    
+    <p class="mt-4 text-sm">
+        Categoria: {{ $article->categories()->first()->name }}
+    </p>
 
     <form action="{{ route('articles.index') }}">  
       <x-form.submit-button text="Voltar" />
